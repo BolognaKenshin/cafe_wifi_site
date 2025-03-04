f5028052e3cbd08ec66e9d1b5fdb1cafea12ca55
Carl <cmillardv77@gmail.com>
2025-03-04 15:21:14 -0500

    Master Branch

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, redirect, render_template, request, url_for
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
-
 from forms import SearchCafeForm, AddCafeForm
 import os
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
